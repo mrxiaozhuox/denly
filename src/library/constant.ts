b22aab12d73f3b4f903a0752a9e71f0428e7fdfc
mrxiaozhuox <mrxzx@wwsg18.top>
2021-05-13 21:38:50 +0800

    commit temp

--- src/library/constant.ts
+++ src/library/constant.ts
@@ -14,3 +14,35 @@ export const _dirname = (() => {
  * Get Denly framework version
  */
 export const _version = "V0.21";
+
+/**
+ * @description Get Platform Separator
+ */
+export const _separator: string = (() => {
+    if (Deno.build.os == "windows") {
+        return "\\";
+    } else {
+        return "/";
+    }
+})();
+
+/**
+ * @description Get Temp Directory Path
+ */
+export const _tempdir: string = (() => {
+    try {
+        let temp: string = Deno.makeTempDirSync();
+        let index = temp.lastIndexOf(_separator);
+
+        let folder: string = temp.substring(0, index);
+
+        Deno.removeSync(temp, { recursive: true });
+
+        return folder;
+
+    } catch (error) {
+        return _dirname + "/temp/";
+    }
+
+    return "";
+})();
